0cbb35702e7c27d77e610458a398df7afd7f3c74
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-23 21:44:57 +0200

    Implemented constant.

--- src/main/kotlin/BoxVolumeCalculator.kt
+++ src/main/kotlin/BoxVolumeCalculator.kt
@@ -1,6 +1,8 @@
+private const val SIDE_VALUE_TEST = 3
+
 fun main() {
-    println(getBoxVolume(3,3,3))
-    println(getBoxVolume(3,3))
+    println(getBoxVolume(SIDE_VALUE_TEST,SIDE_VALUE_TEST,SIDE_VALUE_TEST))
+    println(getBoxVolume(SIDE_VALUE_TEST,SIDE_VALUE_TEST))
 }
 
 fun getBoxVolume(length: Int, width: Int, height: Int = 1) : Int {
