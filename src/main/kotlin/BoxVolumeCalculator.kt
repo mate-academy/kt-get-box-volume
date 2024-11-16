f67224158c8ed44237662275ffdf3967f8143de6
Anatolii Nazarov <anatoly_n@ukr.net>
2024-11-16 20:41:53 +0200

    For check

--- src/main/kotlin/BoxVolumeCalculator.kt
+++ src/main/kotlin/BoxVolumeCalculator.kt
@@ -1,1 +1,13 @@
 // create a function and implement it
+fun main() {
+  val length = 5
+  val width = 10
+  val height = 2
+
+  val volume = getBoxVolume(length, width, height)
+  println("Volume: $volume")
+}
+
+fun getBoxVolume(length: Int, width: Int, height: Int = 1): Int {
+  return length * width * height
+}
