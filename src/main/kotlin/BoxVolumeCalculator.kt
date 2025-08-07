3c9e65e4c3bf72e353f86e25beaea1a2263db3b8
Dmytro Sokolovskyi <dm.sokolovskyi@gmail.com>
2025-08-06 21:46:27 -0400

    added boxVolumeCalculator implementation

--- src/main/kotlin/BoxVolumeCalculator.kt
+++ src/main/kotlin/BoxVolumeCalculator.kt
@@ -1,1 +1,4 @@
-// create a function and implement it
+fun getBoxVolume(length: Int, width: Int, height: Int = 1) : Int {
+    val boxDimensions: (Int, Int, Int) -> Int = { length, width, height -> length.times(width).times(height) }
+    return boxDimensions(length, width, height)
+}
